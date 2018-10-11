444cf65cdf7126cf1d83dc741800824bd82b2294
CloudSen <www.402645063@gmail.com>
2018-10-11 22:24:14 +0800

    bootstrap 测试

--- cloudsen_blog/urls.py
+++ cloudsen_blog/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.go_home, name='cloudsen_home')
+    # home page route
+    path('', views.go_home_page, name='go_cloudsen_home'),
+
+    # blog page route
+    path('blog/', views.go_blog_page, name='go_blog'),
+    # blog articles route
+    path('blog/article/<int:article_pk>', views.go_article_detail_page, name='go_article_detail'),
+    # blog tags page route
+    path('blog/tag/<str:tag_name>/articles/', views.go_same_tag_articles_page, name='go_same_tag_articles'),
 ]
--- cloudsen_blog/views.py
+++ cloudsen_blog/views.py
@@ -1,18 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+import cloudsen_blog.models as models
 
 
-def go_home(request: HttpRequest):
-    return render(request, 'cloudsen-index.html')
+def go_home_page(request: HttpRequest):
+    """redirect to home page"""
+    return render(request, 'cloudsen_blog/cloudsen-index.html')
 
 
-def list_all_articles(request: HttpRequest):
+def go_blog_page(request: HttpRequest):
+    """redirect to blog page"""
+    articles = list_all_articles()
+    context = {'articles': articles}
+    return render(request, 'cloudsen_blog/blog.html', context)
+
+
+def go_article_detail_page(request: HttpRequest, article_pk: int):
+    """redirect to article's detail page"""
+    article = models.Article.objects.get(pk=article_pk)
+    context = {'article': article}
+    return render(request, 'cloudsen_blog/article-detail.html', context)
+
+
+def go_same_tag_articles_page(request: HttpRequest, tag_name: str):
+    """redirect to page that shows all articles with same tag"""
     pass
 
 
-def show_article_detail(request: HttpRequest, article_pk: int):
+def list_all_articles():
+    """get a list of all my articles"""
+    return models.Article.objects.all()
+
+
+def get_article_detail(article_pk: int):
+    """retuen an article's detail, searching by primary key"""
     pass
 
 
-def show_same_tag_article(request: HttpRequest, article_tag: str):
+def list_same_tag_articles(tag_name: str):
+    """return a list of articles with same tag"""
     pass
