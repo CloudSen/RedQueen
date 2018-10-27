0c3c2fd5ea1c9705c250b5d0edb7315795e27e08
cloudsen <www.402645063@gmail.com>
2018-10-28 02:40:14 +0800

    add paginator

--- cloudsen_blog/views.py
+++ cloudsen_blog/views.py
@@ -13,10 +13,12 @@ def go_home_page(request: HttpRequest):
 def go_blog_page(request: HttpRequest):
     """redirect to blog page"""
     page_num = request.GET.get('page_num', 1)
-    page = list_latest_2_month_articles(page_num)
-    tags = list_all_tags()
-    my_ideas = list_all_my_idea()
-    context = {'latest_articles_page': page, 'tags': tags, 'ideas': my_ideas}
+    page, page_range = page_latest_2_month_articles(page_num)
+    context = {
+        'page_range': page_range,
+        'page': page,
+        'tags': list_all_tags(),
+        'ideas': list_all_my_idea()}
     return render(request, 'cloudsen_blog/blog/blog.html', context)
 
 
@@ -42,12 +44,16 @@ def list_all_articles():
     return models.Article.objects.all()
 
 
-def list_latest_2_month_articles(page_number: int = 1):
+def page_latest_2_month_articles(page_number: int = 1):
+    """
+    paginate the articles published within two month
+
+    :param page_number: expect page number
+    :return: page and range
+    """
     latest_2_month_date = timezone.now() - timezone.timedelta(days=60)
-    latest_articles = models.Article.objects.filter(update_time__gte=latest_2_month_date).order_by('update_time')
-    paginator = Paginator(latest_articles, 2)
-    page = paginator.get_page(page_number)
-    return page
+    latest_articles = models.Article.objects.filter(update_time__gte=latest_2_month_date).order_by('-update_time')
+    return paginate_data(latest_articles, 10, page_number)
 
 
 def get_article_detail(article_pk: int):
@@ -66,3 +72,26 @@ def list_all_tags():
 
 def list_all_my_idea():
     return models.MyIdea.objects.all()
+
+
+def paginate_data(source_data, per_page: int, page_number: int):
+    """
+    only display two pages numbers before and after current page number
+
+    :param source_data: object list
+    :param per_page: how many data per page
+    :param page_number: current page number
+    :return: a tuple of page object and processed range
+    """
+    paginator = Paginator(source_data, per_page)
+    page = paginator.get_page(page_number)
+    page_range = [x for x in range(page.number - 2, page.number + 3) if 0 < x <= page.paginator.num_pages]
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if page.paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != page.paginator.num_pages:
+        page_range.append(page.paginator.num_pages)
+    return page, page_range
