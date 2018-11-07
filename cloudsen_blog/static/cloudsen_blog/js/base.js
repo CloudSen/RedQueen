272dfca1674bf9def0d94b23b2b39ff03aae6785
CloudSen <www.402645063@gmail.com>
2018-11-07 21:38:04 +0800

    add particles effect

--- cloudsen_blog/static/cloudsen_blog/js/base.js
+++ cloudsen_blog/static/cloudsen_blog/js/base.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
+
     $('body').bootstrapMaterialDesign()
+
+    particlesJS.load('particles-js', '/static/redqueen/js/particles/particlesjs-config.json', function () {
+        console.debug('particles.js config loaded...')
+    })
 })
