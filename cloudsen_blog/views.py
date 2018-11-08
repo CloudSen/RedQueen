c452c01c80c9184b3a885f054947dad69c7338c9
CloudSen <www.402645063@gmail.com>
2018-11-09 04:27:07 +0800

    Update cloudsen_blog/views.py
    add about me  and friend links

--- cloudsen_blog/views.py
+++ cloudsen_blog/views.py
@@ -18,7 +18,9 @@ def go_blog_page(request: HttpRequest):
         'page_range': page_range,
         'page': page,
         'tags': list_all_tags(),
-        'ideas': list_all_my_idea()}
+        'ideas': list_all_my_idea(),
+        'friends_link': list_friends_link(),
+    }
     return render(request, 'cloudsen_blog/blog/blog.html', context)
 
 
@@ -54,12 +56,23 @@ def go_same_tag_articles_page(request: HttpRequest, tag_pk: int):
 def go_site_timeline_page(request: HttpRequest):
     timelines = list_all_site_timeline()
     context = {'timelines': timelines}
-    return render(request, 'cloudsen_blog/timeline/site_timeline.html', context)
+    return render(request, 'cloudsen_blog/timeline/site-timeline.html', context)
+
+
+def go_about_me_page(request: HttpRequest):
+    about_me = get_about_me()
+    context = {'about_me': about_me}
+    return render(request, 'cloudsen_blog/about-me/about-me.html', context)
+
+
+def go_monero_mine(request: HttpRequest):
+    context = {}
+    return render(request, 'cloudsen_blog/monero/monero.html', context)
 
 
 def list_all_articles():
     """get a list of all my articles"""
-    return models.Article.objects.all()
+    return models.Article.objects.filter(is_deleted=False)
 
 
 def paginate_latest_2_month_articles(page_number: int = 1):
@@ -70,31 +83,40 @@ def paginate_latest_2_month_articles(page_number: int = 1):
     :return: page and range
     """
     latest_2_month_date = timezone.now() - timezone.timedelta(days=60)
-    latest_articles = models.Article.objects.filter(update_time__gte=latest_2_month_date).order_by('-update_time')
+    latest_articles = models.Article.objects.filter(update_time__gte=latest_2_month_date, is_deleted=False) \
+        .order_by('-update_time')
     return paginate_data(latest_articles, 10, page_number)
 
 
 def get_article_detail(article_pk: int):
     """retuen an article's detail, searching by primary key"""
-    return get_object_or_404(models.Article, pk=article_pk)
+    return get_object_or_404(models.Article, pk=article_pk, is_deleted=False)
 
 
 def paginate_same_tag_articles(tag_pk: int, page_number: int = 1):
     """return a list of articles with same tag"""
-    articles = get_list_or_404(models.Article, tag_id=tag_pk)
+    articles = get_list_or_404(models.Article, tag_id=tag_pk, is_deleted=False)
     return paginate_data(articles, 10, page_number)
 
 
 def list_all_tags():
-    return models.Tag.objects.all()
+    return models.Tag.objects.filter(is_deleted=False)
 
 
 def list_all_my_idea():
-    return models.MyIdea.objects.all()
+    return models.MyIdea.objects.filter(is_deleted=False)
 
 
 def list_all_site_timeline():
-    return models.SiteTimeLine.objects.all().order_by('create_time')
+    return models.SiteTimeLine.objects.filter(is_deleted=False).order_by('create_time')
+
+
+def get_about_me():
+    return models.AboutMe.objects.filter(is_deleted=False).first()
+
+
+def list_friends_link():
+    return models.FriendsLink.objects.filter(is_deleted=False)
 
 
 def paginate_data(source_data, per_page: int, page_number: int):
