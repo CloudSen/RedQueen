eebf9406f78e184d075515ff3be9eefa71fc914f
CloudSen <www.402645063@gmail.com>
2018-11-11 02:39:55 +0800

    add more functionality
    1. use ES6;
    2. add CSRF common function for jquery ajax;

--- cloudsen_blog/static/cloudsen_blog/js/base.js
+++ cloudsen_blog/static/cloudsen_blog/js/base.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
+$(() => {
     // bootstrap material 需要的
     $('body').bootstrapMaterialDesign()
 
     // 加载粒子效果
-    particlesJS.load('particles-js', '/static/redqueen/js/particles/particlesjs-config.json', function () {
+    particlesJS.load('particles-js', '/static/redqueen/js/particles/particlesjs-config.json', () => {
         console.debug('particles.js config loaded...')
     })
 
@@ -19,19 +19,53 @@ $(document).ready(function () {
     console.debug('document height: ' + $(document).height())
     console.groupEnd()
 
-    // 回到顶部按钮
     /* 按下GoTop按鈕時的事件 */
-    $('#gotop').click(function(){
-        $('html,body').animate({ scrollTop: 0 }, 'slow')
+    $('#gotop').click(() => {
+        $('html,body').animate({scrollTop: 0}, 'slow')
         return false
-    });
+    })
 
     /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() > 400){
+    $(window).scroll(() => {
+        if ($(this).scrollTop() > 400) {
             $('#gotop').fadeIn()
         } else {
             $('#gotop').fadeOut()
         }
-    });
+    })
+
+    // CSRF
+    function getCookie(name) {
+        let cookieValue = null
+        if (document.cookie && document.cookie !== '') {
+            let cookies = document.cookie.split(';')
+            for (let i = 0; i < cookies.length; i++) {
+                let cookie = jQuery.trim(cookies[i])
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+                    break
+                }
+            }
+        }
+        return cookieValue
+    }
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method))
+    }
+
+    $.ajaxSetup({
+        beforeSend: (xhr, settings) => {
+            let $captchaMassage = $('#captcha-message')
+            let csrftoken = getCookie('csrftoken')
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken)
+            }
+            if ($captchaMassage) {
+                $captchaMassage.text('数据处理中...请稍后~')
+            }
+        }
+    })
 })
