8b79a94554707c52a31b5b39cd6437f0191c03f9
CloudSen <www.402645063@gmail.com>
2018-11-20 03:50:55 +0800

    update some cong

--- RedQueen/gunicorn_conf.py
+++ RedQueen/gunicorn_conf.py
@@ -5,7 +5,7 @@ bind = 'unix:/home/cloudsen/work/deploy/webservers/sockets/nginx-gunicorn.sock'
 # bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gevent'
-errorlog = '/home/cloudsen/work/deploy/webservers/gunicorn/guicorn.error.log'
+errorlog = '/home/cloudsen/work/deploy/webservers/gunicorn/gunicorn.error.log'
 accesslog = '/home/cloudsen/work/deploy/webservers/gunicorn/gunicorn.access.log'
 proc_name = 'gunicorn_myblog'
 # Daemonize the Gunicorn process, use command line config
